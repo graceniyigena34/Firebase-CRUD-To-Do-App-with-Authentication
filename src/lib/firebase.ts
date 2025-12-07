7238960345b341cee7b1a8d8bb52cfa8cf06ca4c
graceniyigena34 <graceniyigena34@gmail.com>
2025-12-07 07:09:49 +0200

    Resolved  the conflict located on the firebase.ts page

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA13Q-fIreIbVaqHcE81jeaEtTBzyQqo48",
+  authDomain: "crud-project-d1487.firebaseapp.com",
+  projectId: "crud-project-d1487",
+  storageBucket: "crud-project-d1487.firebasestorage.app",
+  messagingSenderId: "322840164408",
+  appId: "1:322840164408:web:2c3578197db4bb3a110ecd"
 };
 
 const app = initializeApp(firebaseConfig);
